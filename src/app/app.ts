c098e634c9f231c3e3490d4f4310ff6018dbc24d
“[Marlon <marlondossantosklain@gmail.com>
2025-11-25 20:16:12 -0300

    created the proto

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Hero } from './components/hero/hero';
+import { Products } from './components/products/products';
+import { About } from './components/about/about';
+import { Reviews } from './components/reviews/reviews';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Hero, Products, About, Reviews],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected title = 'beer-disk';
+  protected isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
